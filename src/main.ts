571c7409da3ae5cb4d8ca09d30822900ec372002
Ubuntu <ubuntu@ip-172-31-10-253.ec2.internal>
2024-02-28 18:21:38 +0000

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
+  app.enableCors(); // Cors para app nextjs. Habilita peticiones del exterior
   await app.listen(3001);
 }
 bootstrap();
